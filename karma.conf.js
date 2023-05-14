8b9c2d482f27bfe687b0df68f2002b8987e57081
VitaliyNarytnyk <vitaliy.narytnyk@gmail.com>
2023-05-14 23:51:24 +0300

    firebase configuration

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/kvremstroy'),
+      dir: require('path').join(__dirname, './coverage/colorCube'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'kvremstroy'`, () => {
+  it(`should have as title 'colorCube'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('kvremstroy');
+    expect(app.title).toEqual('colorCube');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('kvremstroy app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('colorCube app is running!');
   });
 });
